28e5c22e1305f826c7d50b7d57ac36253473c503
Zekret <benjaminconcha5@gmail.com>
2022-06-16 19:18:08 -0400

    update: classname in NavBar if is active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../style/navbar.css'
 
 const NavBar = () => {
     return (
         <div>
             <ul>
                 <li>
-                    <NavLink to='/about'>About</NavLink>
+                    <NavLink
+                        className={({ isActive }) => (isActive ? "active" : "")}
+                        to='/'
+                    >
+                        Home
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/users'>Users</NavLink>
+                    <NavLink
+                        className={({ isActive }) => (isActive ? "active" : "")}
+                        to='/about'>
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        className={({ isActive }) => (isActive ? "active" : "")}
+                        to='/users'
+                    >
+                        Users
+                    </NavLink>
                 </li>
             </ul>
         </div>
